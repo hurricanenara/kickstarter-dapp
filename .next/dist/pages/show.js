1bca1513ae7b0bd8255870e3bab1c4e8021c741e
naracodes <naralee0124@gmail.com>
2021-05-05 20:32:11 -0400

    set up next.js

---
+++ .next/dist/pages/show.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/nara/Library/Mobile Documents/com~apple~CloudDocs/kickstart/pages/show.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, "show page");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL3Nob3cuanMiXSwibmFtZXMiOlsiUmVhY3QiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU8sQUFFUDs7Ozs7Ozs7a0JBQWUsWUFBTSxBQUNuQjt5QkFBTyxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsR0FBQSxFQUFQLEFBQU8sQUFDUjtBQUZEIiwiZmlsZSI6InNob3cuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL25hcmEvTGlicmFyeS9Nb2JpbGUgRG9jdW1lbnRzL2NvbX5hcHBsZX5DbG91ZERvY3Mva2lja3N0YXJ0In0=
