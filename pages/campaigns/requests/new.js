75c7c5a5278bca14b1cdb88619fa38a06a755c0c
naracodes <naralee0124@gmail.com>
2021-05-08 17:55:51 -0400

    complete request form

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Layout from "../../../components/Layout";
+import { Form, Message, Button, Input } from "semantic-ui-react";
+import Summary from "../../../ethereum/campaign";
+import web3 from "../../../ethereum/web3";
+import { Link, Router } from "../../../routes";
+
+class RequestNew extends React.Component {
+  state = {
+    value: "",
+    description: "",
+    recipient: "",
+  };
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    return { address };
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h3>Create a Request</h3>
+        <Form>
+          <Form.Field>
+            <label>Description</label>
+            <Input
+              value={this.state.description}
+              onChange={(e) => this.setState({ description: e.target.value })}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Value in Ether</label>
+            <Input
+              value={this.state.value}
+              onChange={(e) => this.setState({ value: e.target.value })}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Recipient</label>
+            <Input
+              value={this.state.recipient}
+              onChange={(e) => this.setState({ recipient: e.target.value })}
+            />
+          </Form.Field>
+          <Button primary>Create</Button>
+        </Form>
+      </Layout>
+    );
+  }
+}
+
+export default RequestNew;
