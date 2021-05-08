246a9d6d4c3ca6b4455c63558b839d75c3262bf0
naracodes <naralee0124@gmail.com>
2021-05-08 14:17:04 -0400

    add error bahavior

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -8,11 +8,13 @@ class ContributeForm extends React.Component {
   state = {
     value: "",
     loading: false,
+    errorMessage: "",
   };
 
   onSubmit = async (e) => {
     e.preventDefault();
     const campaign = Summary(this.props.address);
+    this.setState({ loading: true, errorMessage: "" });
     try {
       const accounts = await web3.eth.getAccounts();
       await campaign.methods.contribute().send({
@@ -20,12 +22,15 @@ class ContributeForm extends React.Component {
         value: web3.utils.toWei(this.state.value, "ether"),
       });
       Router.replaceRoute(`/campaigns/${this.props.address}`);
-    } catch (err) {}
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.setState({ loading: false, value: "" });
   };
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
@@ -35,7 +40,10 @@ class ContributeForm extends React.Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>Contribute</Button>
+        <Message error header="Oops!" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
+          Contribute
+        </Button>
       </Form>
     );
   }
