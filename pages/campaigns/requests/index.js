1982bdbb0c6374ce2bff9d149f26a1441b64d190
naracodes <naralee0124@gmail.com>
2021-05-09 11:51:32 -0400

    start building out requests table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
+import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends React.Component {
   static async getInitialProps(props) {
     const { address } = props.query;
-    return { address };
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestCount().call();
+
+    const requests = await Promise.all(
+      Array(+requestCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    return { address, requests, requestCount };
   }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          address={this.props.address}
+          request={request}
+        />
+      );
+    });
+  }
+
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <h3>Request List</h3>
@@ -17,6 +45,20 @@ class RequestIndex extends React.Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
       </Layout>
     );
   }
